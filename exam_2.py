70226f1885aa8a9a6db8deb20059ef49ac670dc2
JohnnyMarlison <renat.enokaev@mail.ru>
2020-03-16 20:59:40 +0300

    finished exam_2.py

--- exam_2.py
+++ exam_2.py
@@ -1,8 +1,9 @@
 import pygame
+import sys
 import random
 
 pygame.init()
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
 display = pygame.display.set_mode((500, 500))
 apple_pos = [random.randint(1, 50) * 10, random.randint(1, 50) * 10]
 snake_pos = [[250, 250], [240, 250], [230, 250]]
@@ -24,15 +25,65 @@ def coll_with_bound(snake_head):
 
 
 def coll_with_self(snake_pos):
-
+	for i in snake_pos[1:]:
+		if snake_pos[0] == i:
+			return 1
 
 
 def generate(snake_head, snake_pos, apple_pos, but_dir, score):
-
+	if but_dir == 1:
+		snake_head[0] += 10
+	elif but_dir == 0:
+		snake_head[0] += 10
+	elif but_dir == 2:
+		snake_head[1] += 10
+	elif but_dir == 3:
+		snake_head[1] -= 10
+	else:
+		pass
 
 
 def display_snake_apple(display, snake_pos, apple_pos):
+	for i in snake_pos: 
+		pygame.draw.rect(display, (255, 255, 255), pygame.Rect(i[0], i[1], 10, 10))
 
+	pygame.draw.rect(display, (255, 0, 0), pygame.Rect(apple_pos[0], apple_pos[1], 10, 10))
 
 
 def play_game(snake_head, snake_pos, apple_pos, but_dir, score):
+	crashed = False
+	prev_but_dir = 1
+
+	while crashed is not True:
+		for event in pygame.event.get():
+			keys = pygame.key.get_pressed()
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+			if keys[pygame.K_LEFT] and prev_but_dir != 1:
+				but_dir = 0
+			elif keys[pygame.K_RIGHT] and prev_but_dir != 0:
+				but_dir = 1
+			elif keys[pygame.K_UP] and prev_but_dir != 2:
+				but_dir = 3
+			elif keys[pygame.K_DOWN] and prev_but_dir != 3:
+				but_dir = 2
+			else:
+				but_dir = but_dir
+
+		display.fill((0, 0, 0))
+		display_snake_apple(display, snake_pos, apple_pos)
+
+
+		snake_pos, apple_pos, score = generate(snake_head, snake_pos, apple_pos, but_dir, score)
+		pygame.display.set_caption("SCORE: " + str(score))
+		pygame.display.update()
+		prev_but_dir = but_dir
+		if death(snake_pos) == 1:
+			crashed = True
+		cloc.tick(20)
+		pygame.display.update()
+
+	return score
+
+final_score = play_game(snake_head, snake_pos, apple_pos, 1, score)
